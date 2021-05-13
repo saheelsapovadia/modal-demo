b59627db3f57f4aa63bad852e88a3ebd2c235ea2
saheelsapovadia <saheelsapovadia23@gmail.com>
2021-05-13 22:26:09 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { GlobalStyle } from './globalStyles';
+import './App.css';
+import styled from 'styled-components';
+import { Modal } from './Components/Modal';
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+  return (
+    <Container>
+      <Button onClick={openModal}>Sign up</Button>
+      <Modal showModal={showModal} setShowModal={setShowModal}></Modal>
+      <GlobalStyle />
+    </Container>
+  );
+}
+
+export default App;
